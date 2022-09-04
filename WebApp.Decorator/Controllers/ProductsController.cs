ff78e84fa7d3cfe4cf3a3c66f4b3d415ecb95c34
salihavci <cacabeystjr@gmail.com>
2022-09-04 12:19:49 +0300

    Decorator Design Pattern

---
+++ WebApp.Decorator/Controllers/ProductsController.cs
@@ -0,0 +1,162 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using WebApp.Decorator.Models;
+using WebApp.Decorator.Repositories;
+
+namespace WebApp.Decorator.Controllers
+{
+    [Authorize]
+    public class ProductsController : Controller
+    {
+        private readonly IProductRepository productRepository;
+
+        public ProductsController(IProductRepository productRepository)
+        {
+            this.productRepository = productRepository;
+        }
+
+        // GET: Products
+        public async Task<IActionResult> Index()
+        {
+            var userId = User.Claims.First(m => m.Type == ClaimTypes.NameIdentifier).Value;
+
+            /*
+             İf memory'de var mı?
+             varsa memory'den dön,
+             yoksa veritabanından dön,
+             memory'e kaydet,
+             datayı dön
+             */
+
+            return View(await this.productRepository.GetAll(userId));
+        }
+
+        // GET: Products/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var product = await this.productRepository.GetById(id.Value);
+            if (product == null)
+            {
+                return NotFound();
+            }
+
+            return View(product);
+        }
+
+        // GET: Products/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Products/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,Name,Price,Stock")] Product product)
+        {
+            if (ModelState.IsValid)
+            {
+                product.UserId = User.Claims.First(m => m.Type == ClaimTypes.NameIdentifier).Value;
+                await this.productRepository.Save(product);
+                return RedirectToAction(nameof(Index));
+            }
+            return View(product);
+        }
+
+        // GET: Products/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var product = await this.productRepository.GetById(id.Value);
+            if (product == null)
+            {
+                return NotFound();
+            }
+            return View(product);
+        }
+
+        // POST: Products/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Name,Price,Stock,UserId")] Product product)
+        {
+            if (id != product.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    await this.productRepository.Update(product);
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!ProductExists(product.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(product);
+        }
+
+        // GET: Products/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var product = await this.productRepository.GetById(id.Value);
+            if (product == null)
+            {
+                return NotFound();
+            }
+
+            return View(product);
+        }
+
+        // POST: Products/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var product = await this.productRepository.GetById(id);
+            await this.productRepository.Remove(product);
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool ProductExists(int id)
+        {
+            return this.productRepository.GetById(id) != null;
+        }
+    }
+}
---
+++ WebApp.Decorator/Repositories/Decorator/ProductRepositoryLoggingDecorator.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.Extensions.Logging;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using WebApp.Decorator.Models;
+
+namespace WebApp.Decorator.Repositories.Decorator
+{
+    public class ProductRepositoryLoggingDecorator : BaseProductRepositoryDecorator
+    {
+        private readonly ILogger<ProductRepositoryLoggingDecorator> _logger;
+
+        public ProductRepositoryLoggingDecorator(IProductRepository productRepository, ILogger<ProductRepositoryLoggingDecorator> logger) : base(productRepository)
+        {
+            _logger = logger;
+        }
+        public override Task<List<Product>> GetAll()
+        {
+            _logger.LogInformation("GetAll metodu çalıştı.");
+            return base.GetAll();
+        }
+        public override Task<List<Product>> GetAll(string userId)
+        {
+            _logger.LogInformation("GetAll(userId) metodu çalıştı.");
+            return base.GetAll(userId);
+        }
+        public override Task<Product> Save(Product product)
+        {
+
+            _logger.LogInformation("Save metodu çalıştı.");
+            return base.Save(product);
+        }
+        public override Task Update(Product product)
+        {
+            _logger.LogInformation("Update metodu çalıştı.");
+            return base.Update(product);
+        }
+        public override Task Remove(Product product)
+        {
+            _logger.LogInformation("Remove metodu çalıştı.");
+            return base.Remove(product);
+        }
+    }
+}
